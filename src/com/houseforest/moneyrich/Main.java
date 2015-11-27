338983f106e641f64df98d7c2f7cf423ad78f058
shmup73 <t.hauswald@yahoo.de>
2015-11-27 12:53:09 +0100

    Added username, password prompt at the beginning of the program.

--- src/com/houseforest/moneyrich/Main.java
+++ src/com/houseforest/moneyrich/Main.java
@@ -1,5 +1,9 @@
 package com.houseforest.moneyrich;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 
 public class Main {
@@ -9,16 +13,41 @@ public class Main {
 
     public Main() {
 
-        final String username = "thauswald";
-        final String password = "bucherregal123";
+        String username = "", password = "";
 
         browser = new Browser(java.nio.charset.StandardCharsets.UTF_8.name());
         depot = new Depot();
-        depot.login(browser, username, password);
+
+        try {
+            System.out.println("Welcome to the moneyrich Depot Manager!");
+            Console console = System.console();
+            username = readLine("Username: ");
+            password = String.valueOf(readPassword("Password: "));
+            depot.login(browser, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         depot.update(browser);
     }
 
     public static void main(String[] args) {
         new Main();
+    }
+
+    private String readLine(String format, Object... args) throws IOException {
+        if (System.console() != null) {
+            return System.console().readLine(format, args);
+        }
+        System.out.print(String.format(format, args));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+    }
+
+    private char[] readPassword(String format, Object... args)
+            throws IOException {
+        if (System.console() != null)
+            return System.console().readPassword(format, args);
+        return this.readLine(format, args).toCharArray();
     }
 }
